1a6c28d28c2e98b26396b5093d489612698586a6
Syed Armash Hussain <armashash@gmail.com>
2022-06-25 00:07:10 +0500

    refactor: home page code refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll/modules";
 import Main from "../components/layout/Main";
+import TypeWriter from "typewriter-effect";
 
 function Home() {
   return (
     <Main name="home">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">Hi, My name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Armash
+        <p className="text-white">Hi, My name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-white">
+          Syed Armash Hussain
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
-        </h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
-          iusto.
-        </p>
+        <div className="text-4xl sm:text-7xl py-4 font-bold text-white">
+          <TypeWriter
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(2500)
+                .typeString("I'm a ")
+                .typeString("Student")
+                .deleteChars(7)
+                .typeString("Software Engineer")
+                .pause()
+                .start();
+            }}
+          />
+        </div>
 
         <div className="">
-          <button className="text-white group border-2 rounded-lg px-6 py-3 my-2 flex items-center hover:bg-pink-600  hover:border-pink-600 duration-200">
-            View Work
+          <button className="text-white group border-2 rounded-lg px-6 py-3 my-2 flex items-center hover:bg-white  hover:border-white hover:text-black duration-200">
+            <Link to="work" smooth={true} duration={500}>
+              View Work
+            </Link>
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-2 text-xl" />
             </span>
