d4524bc65d0785670d71fbb5743b5ec4865efe20
Syed Armash Hussain <armashash@gmail.com>
2022-07-30 18:46:26 +0500

    feat: added dynamic title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Home from "./pages/Home";
 import Navbar from "./pages/Navbar";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
+import {APP_TITLE} from "./content";
 
 function App() {
+  const title = APP_TITLE;
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
   return (
     <>
       <Navbar />
